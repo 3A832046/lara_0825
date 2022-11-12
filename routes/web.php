c34e53f802584c516e73aca41bbbf521c64b91b8
Chen Ching Ling <3a832046@gm.student.ncut.edu.tw>
2022-11-13 01:47:50 +0800

    用save的方式新增資料

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,7 +16,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect(route('posts.index'));
+    //return redirect(route('posts.index'));
+    $post=new Post();
+    $post->title='test title';
+    $post->content='test content';
+    $post->save();
 });
 
 Route::get('posts',[PostController::class,'index'])->name('posts.index');
