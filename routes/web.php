eb7f9532ae63302dbd16f6eddaf5d198bc52c8ce
Chen Ching Ling <3a832046@gm.student.ncut.edu.tw>
2022-11-13 13:46:07 +0800

    查詢資料

--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,38 @@ use App\Models\Post;
 
 Route::get('/', function () {
     //return redirect(route('posts.index'));
+
+    //---使用save方法---
     //$post=new Post();
     //$post->title='test title';
     //$post->content='test content';
     //$post->save();
-    Post::create([
+
+    //---使用create方法---
+    /*Post::create([
         'title'=>'created title',
         'content'=>'created content',
-    ]);
+    ]);*/
+
+    //---使用find方法---
+    /*$post=Post::find(1);
+    echo '標題：'.$post->title.'<br>';
+    echo '內容：'.$post->content.'<br>';
+    dd($post);*/
+
+    //---使用all方法---
+    /*$posts=Post::all();
+    foreach ($posts as $post){
+        echo '編號：'.$post->id.'<br>';
+        echo '標題：'.$post->title.'<br>';
+        echo '內容：'.$post->content.'<br>';
+        echo '張貼時間：：'.$post->created_at.'<br>';
+    }
+    dd($posts);*/
+
+    //---練習條件式---
+    $posts=Post::where('id','<',10)->orderBy('id','DESC')->get();
+    dd($posts);
 });
 
 Route::get('posts',[PostController::class,'index'])->name('posts.index');
