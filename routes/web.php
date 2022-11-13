7ba8d0ea4a90d55d68a475e1bd073f3e2f4c8274
Chen Ching Ling <3a832046@gm.student.ncut.edu.tw>
2022-11-13 13:56:13 +0800

    更新資料：使用update方法

--- routes/web.php
+++ routes/web.php
@@ -18,37 +18,50 @@ use App\Models\Post;
 Route::get('/', function () {
     //return redirect(route('posts.index'));
 
-    //---使用save方法---
-    //$post=new Post();
-    //$post->title='test title';
-    //$post->content='test content';
-    //$post->save();
+    //-----新增資料-----
+        //-使用save方法-
+        //$post=new Post();
+        //$post->title='test title';
+        //$post->content='test content';
+        //$post->save();
 
-    //---使用create方法---
-    /*Post::create([
-        'title'=>'created title',
-        'content'=>'created content',
-    ]);*/
+        //-使用create方法-
+        /*Post::create([
+            'title'=>'created title',
+            'content'=>'created content',
+        ]);*/
 
-    //---使用find方法---
-    /*$post=Post::find(1);
-    echo '標題：'.$post->title.'<br>';
-    echo '內容：'.$post->content.'<br>';
-    dd($post);*/
-
-    //---使用all方法---
-    /*$posts=Post::all();
-    foreach ($posts as $post){
-        echo '編號：'.$post->id.'<br>';
+    //-----查詢資料-----
+        //-使用find方法-
+        /*$post=Post::find(1);
         echo '標題：'.$post->title.'<br>';
         echo '內容：'.$post->content.'<br>';
-        echo '張貼時間：：'.$post->created_at.'<br>';
-    }
-    dd($posts);*/
+        dd($post);*/
 
-    //---練習條件式---
-    $posts=Post::where('id','<',10)->orderBy('id','DESC')->get();
-    dd($posts);
+        //-使用all方法-
+        /*$posts=Post::all();
+        foreach ($posts as $post){
+            echo '編號：'.$post->id.'<br>';
+            echo '標題：'.$post->title.'<br>';
+            echo '內容：'.$post->content.'<br>';
+            echo '張貼時間：：'.$post->created_at.'<br>';
+        }
+        dd($posts);*/
+
+        //-練習條件式-
+        /*$posts=Post::where('id','<',10)->orderBy('id','DESC')->get();
+        dd($posts);*/
+
+
+    //-----更新資料-----
+        //-使用updete方法-
+        $post=Post::find(1);
+        $post->update([
+            'title'=>'updated title',
+            'content'=>'updated content',
+        ]);
+
+
 });
 
 Route::get('posts',[PostController::class,'index'])->name('posts.index');
